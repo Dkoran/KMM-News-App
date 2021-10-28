f294e759552b350e92a341a3911f9ce1f18562cc
DKoran <dkoran2060@gmail.com>
2021-10-28 02:36:08 +0000

    initial commit

---
+++ shared/src/commonMain/kotlin/com/dkoran/newsappkmm/Platform.kt
@@ -0,0 +1,5 @@
+package com.dkoran.newsappkmm
+
+expect class Platform() {
+    val platform: String
+}
---
+++ shared/src/iosMain/kotlin/com/dkoran/newsappkmm/Platform.kt
@@ -0,0 +1,7 @@
+package com.dkoran.newsappkmm
+
+import platform.UIKit.UIDevice
+
+actual class Platform actual constructor() {
+    actual val platform: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
+}
